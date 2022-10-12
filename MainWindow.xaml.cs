fdaee2a5f8709a026454196445ba8ecd0f926b2f
hdmen <hdmen@MATEBOOK-B0RSC>
2022-10-12 10:43:13 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBoxProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public List<OwnColor> ListOfOwnColors { get; set; }
+        public MainWindow()
+        {
+
+            ListOfOwnColors = new List<OwnColor>(); 
+            ListOfOwnColors.Add(new OwnColor());
+          
+            InitializeComponent();
+        }
+    }
+}
---
+++ OwnColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxProject
+{
+    public class OwnColor
+    {
+
+        public string NameOfColor { get; set; }
+        public string NameOfColor_eng { get; set; }
+
+
+
+    }
+}
